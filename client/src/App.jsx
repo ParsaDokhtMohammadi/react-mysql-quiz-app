b092f2e120e987ca928834e6f064dbe2205739d7
parsa dokhtmohammadi <parsadokhtmohammadi.work@gmail.com>
2025-04-19 19:27:11 +0330

    card functionality added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,45 +1,62 @@
-
-import {useState } from 'react'
-import './App.css'
-import axios from "axios"
-import QuestionCard from './components/questionCard'
+import { useState } from "react";
+import "./App.css";
+import axios from "axios";
+import QuestionCard from "./components/questionCard";
 function App() {
-  const [questions , setQuestions] = useState([])
-  const [index , setIndex] = useState(0)
-  const getQuestions = () =>{
-    axios.get('http://localhost:3001/quiz')
-    .then(res=>{
-      setQuestions(res.data)
-    })
-  
-  } 
-  const handleNext = ()=>{
-    if(index !== questions.length - 1){
-      setIndex(index+1)
+  const [questions, setQuestions] = useState([]);
+  const [index, setIndex] = useState(0);
+  const [answers, setAnswers] = useState({});
+  const getQuestions = () => {
+    axios.get("http://localhost:3001/quiz").then((res) => {
+      setQuestions(res.data);
+    });
+  };
+  const handleNext = () => {
+    if (index !== questions.length - 1) {
+      setIndex(index + 1);
     }
-  }
-  const handlePrev = ()=>{
-    if(index !== 0){
-      setIndex(index-1)
+  };
+  const handlePrev = () => {
+    if (index !== 0) {
+      setIndex(index - 1);
     }
-  }
-    
+  };
+  const handleSelect = (value) => {
+    setAnswers((prev) => ({
+      ...prev,
+      [index]: value,
+    }));
+  };
 
   return (
     <>
-    <div className={`${questions.length==0 ? "flex" : "hidden"}  flex-col gap-8 justify-center items-center`}>
-      <h2 className='text-3xl font-bold'>welcome to my awsome quiz app</h2>
-      <button onClick={getQuestions} className='w-[150px]'>start quize</button>
-    </div>
-    <div className={`${questions.length==0 ? "hidden" : "flex"}  flex-col gap-8 justify-center items-center`}>
-      <QuestionCard question={questions[index]}></QuestionCard>
-      <div className='flex gap-52'>
-        <button onClick={handlePrev}>prev</button>
-        <button onClick={handleNext}>next</button>
+      <div
+        className={`${
+          questions.length == 0 ? "flex" : "hidden"
+        }  flex-col gap-8 justify-center items-center`}
+      >
+        <h2 className="text-3xl font-bold">welcome to my awsome quiz app</h2>
+        <button onClick={getQuestions} className="w-[150px]">
+          start quize
+        </button>
       </div>
-    </div>
+      <div
+        className={`${
+          questions.length == 0 ? "hidden" : "flex"
+        }  flex-col gap-8 justify-center items-center`}
+      >
+        <QuestionCard
+          question={questions[index]}
+          selected={answers[index]} // what was chosen for this question
+          onSelect={handleSelect}
+        />
+        <div className="flex gap-52">
+          <button onClick={handlePrev}>prev</button>
+          <button onClick={handleNext}>next</button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/components/QuestionCard.jsx
+++ client/src/components/QuestionCard.jsx
@@ -1,13 +1,63 @@
-import React from 'react'
+import React from "react";
 
-const QuestionCard = (question) => {
-    const title =question.question?.title
-    console.log(question)
+const QuestionCard = ({ question, selected, onSelect }) => {
+  const title = question?.title;
+  const id = question?.id;
+  const option1 = question?.option1;
+  const option2 = question?.option2;
+  const option3 = question?.option3;
+  const option4 = question?.option4;
   return (
-    <div>
-        {title}
-    </div>
-  )
-}
+    <div className="flex flex-col gap-3">
+      <h2>{title}</h2>
 
-export default QuestionCard
+      <div className="flex flex-row justify-between">
+        <div className="flex flex-row gap-2">
+          <input
+            type="radio"
+            name={`answer-${id}`}
+            value={option1}
+            checked={selected === option1}
+            onChange={() => onSelect(option1)}
+          />
+          <label>{option1}</label>
+        </div>
+
+        <div className="flex flex-row gap-2">
+          <input
+            type="radio"
+            name={`answer-${id}`}
+            value={option2}
+            checked={selected === option2}
+            onChange={() => onSelect(option2)}
+          />
+          <label>{option2}</label>
+        </div>
+
+        <div className="flex flex-row gap-2">
+          <input
+            type="radio"
+            name={`answer-${id}`}
+            value={option3}
+            checked={selected === option3}
+            onChange={() => onSelect(option3)}
+          />
+          <label>{option3}</label>
+        </div>
+
+        <div className="flex flex-row gap-2">
+          <input
+            type="radio"
+            name={`answer-${id}`}
+            value={option4}
+            checked={selected === option4}
+            onChange={() => onSelect(option4)}
+          />
+          <label>{option4}</label>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default QuestionCard;
